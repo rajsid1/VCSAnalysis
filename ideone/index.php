cd54d26427601c3b2464446ec2e9213cd96e3956
Rajdeep Siddhapura <raj.webify@gmail.com>
2014-03-02 15:01:46 +0530

    added auto input

--- ideone/index.php
+++ ideone/index.php
@@ -5,9 +5,15 @@
 	$client = new SoapClient("http://ideone.com/api/1/service.wsdl");
 	// calling test function
 	//$testArray = $client->testFunction("rajsid", "qazwsx");
-
-	$subResp = $client->createSubmission("rajsid", "qazwsx", $_POST['source'], 4, isset($_POST['INPUT'])?$_POST['INPUT']:NULL, true, true);
-	$resultArray = array(0 => 'not running – the submission has been created with run parameter set to false', 11 => 'compilation error   –   the   program   could   not   be executed due to compilation error', 12 => 'runtime error – the program finished because of  the runtime error, for example: division by zero, array index out of bounds, uncaught exception', 13 => "time limit exceeded – the program didn't stop before the time limit", 15 => 'success – everything went ok', 17 => 'memory limit exceeded – the program tried to use more memory than it is allowed to', 19 => 'illegal system call – the program tried to call illegal system function', 20 => 'internal   error   –   some   problem   occurred   on ideone.com; try to submit the program again and if that   fails   too,   then   please   contact   us   at contact@ideone.com');
+	
+	preg_match_all("/input/", $_POST['source'], $match);
+	$inputArray = array(0,1,2,3,4,5,6,7,8,9);
+	print_r($match);
+	$input = array_rand($inputArray, count($match[0]));
+	print_r($input);
+	$inputStr=$input;
+	$subResp = $client->createSubmission("rajsid", "qazwsx", $_POST['source'], 4, trim($inputStr), true, true);
+	
 	while(true)
 	{
 		$statResp = $client->getSubmissionStatus("rajsid", "qazwsx", $subResp['link']);
